c92453457aad745bd467438e6c18bce01c2a212f
AlejandroSanchezMonzon <alejandrosanchezmonzon1@gmail.com>
2024-06-01 14:26:21 +0200

    Añadidas fuentes de texto personalizadas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+		extend: {
+		  fontFamily: {
+			'sans': ['F1 Regular', 'sans-serif'],
+			'bold': ['F1 Bold', 'sans-serif'],
+			'italic': ['F1 Italic', 'sans-serif'],
+			'black': ['F1 Black', 'sans-serif'],
+			'wide': ['F1 Wide', 'sans-serif'],
+		  },
+		  colors: {
+			'primary': '#fafafa',
+			'secondary': '#0a0a0a',
+			'accent': '#294643',
+		  },
+		},
+	  },
 	plugins: [],
 }
