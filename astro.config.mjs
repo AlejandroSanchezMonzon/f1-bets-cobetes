09e9407fbba40ea870836968c58408955c11481c
AlejandroSanchezMonzon <alejandrosanchezmonzon1@gmail.com>
2024-06-02 22:11:00 +0200

    Añadido adaptador de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), tailwind()],
   output: "server",
-  site: "https://f1-bets-cobetes.vercel.app/"
+  site: "https://f1-bets-cobetes.vercel.app/",
+  adapter: vercel()
 });
