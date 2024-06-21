35ae342e7cc25bbf4744e7ca4d6ead504f160d79
AlejandroSanchezMonzon <alejandrosanchezmonzon1@gmail.com>
2024-06-21 14:47:36 +0200

    Añadida una semilla para desarrollo

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,26 @@
-import { db } from 'astro:db';
+import { Users, db } from "astro:db";
+import { genSaltSync, hashSync } from "bcrypt-ts";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  const salt = genSaltSync(10);
+
+  const admib = {
+    idUser: crypto.randomUUID(),
+    username: "admin",
+    email: "admin@admin.com",
+    password: hashSync("admin1234", salt),
+    isAdmin: true,
+  };
+
+  const user = {
+    idUser: crypto.randomUUID(),
+    username: "user",
+    email: "user@user.com",
+    password: hashSync("user1234", salt),
+    isAdmin: false,
+  };
+
+  await db.insert(Users).values(admib).onConflictDoNothing();
+  await db.insert(Users).values(user).onConflictDoNothing();
 }
