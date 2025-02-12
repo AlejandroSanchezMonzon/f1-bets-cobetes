5309dc5b0ac28353a008fbc9f683ebe6d2301080
AlejandroSanchezMonzon <alejandrosanchezmonzon1@gmail.com>
2025-02-12 20:58:40 +0100

    Actualiza la obtención de la clave secreta JWT para usar import.meta.env en lugar de process.env

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -6,7 +6,7 @@ export async function checkAdmin(authHeader: string | null): Promise<number | nu
         return null;
     }
     const token = authHeader.slice(7).trim();
-    const secretKey = process.env.JWT_SECRET;
+    const secretKey = import.meta.env.JWT_SECRET;
     if (!secretKey) throw new Error("Missing JWT_SECRET in environment variables");
 
     try {
@@ -29,7 +29,7 @@ export function validateUser(authHeader: string | null): number | null {
         return null;
     }
     const token = authHeader.slice(7).trim();
-    const secretKey = process.env.JWT_SECRET;
+    const secretKey = import.meta.env.JWT_SECRET;
     if (!secretKey) throw new Error("Missing JWT_SECRET in environment variables");
     try {
         const decoded = jwt.verify(token, secretKey) as { userId: number };
