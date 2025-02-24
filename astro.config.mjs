2b02290717e7f069894bc906386f83988204f7c2
AlejandroSanchezMonzon <alejandrosanchezmonzon1@gmail.com>
2025-02-24 21:41:53 +0100

    Actualiza la configuración de Tailwind CSS y elimina el archivo de configuración obsoleto, mejorando la integración con Vite y actualizando las dependencias.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
 import vercel from '@astrojs/vercel';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
   site: "https://f1-bets-cobetes.vercel.app",
 
-  /*vite: {
+  vite: {
     plugins: [tailwindcss()]
-  },*/
-
-  integrations: [tailwind()],
+  },
 
   adapter: vercel()
 });
