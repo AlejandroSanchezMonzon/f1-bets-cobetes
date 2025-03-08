8b357f1faae82ef0816d7af4b7e80159d50ebfe3
AlejandroSanchezMonzon <alejandrosanchezmonzon1@gmail.com>
2025-03-08 21:18:27 +0100

    Elimina las interfaces de tipo no utilizadas: Bet, Pilot, RaceWeekend, Result y User.

--- src/types/Pilot.ts
+++
@@ -1,7 +0,0 @@
-export interface Pilot {
-    id: number;
-    name: string;
-    nationality: string;
-    createdAt: string;
-    deletedAt?: string | null;
-  }
--- src/types/Race.ts
+++
@@ -1,9 +0,0 @@
-export interface RaceWeekend {
-    id: number;
-    roundNumber: number;
-    raceDate: string;
-    raceName: string;
-    raceType: 'gp' | 'sprint';
-    createdAt: string;
-    deletedAt?: string | null;
-  }
